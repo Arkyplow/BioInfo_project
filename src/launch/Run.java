6273fa02e463a1e93710910047869140cae286b0
Jannou <jannoub@gmail.com>
2016-02-25 22:20:11 +0100

    init project

---
+++ src/launch/Run.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package launch;
+
+/**
+ * @author Jannou
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
