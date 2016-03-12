cd062e81861393324be95f6a6b45df9c697b342a
Jannou <jannoub@gmail.com>
2016-03-12 14:29:34 +0100

    création fenetre principale

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,16 +1,32 @@
 package gui;
 
+import javax.swing.*;
+
 /**
+ * fenetre principale
  * @author Jannou
  *
  */
 public class MainFrame {
 
 	/**
-	 * @param args
-	 */
+	 *
+	 * @param args parametre min fenetre principale
+     */
 	public static void main(String[] args) {
-		System.out.println("test");
+		initializeMain();
 	}
 
+	/**
+	 * initialisation fentre principale
+	 */
+	private static void initializeMain() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("BioInformatique");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,600);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+	}
 }
