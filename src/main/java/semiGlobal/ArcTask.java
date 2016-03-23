75e1ce4ca2df3bb2e73e089f2f722aa4f49ef049
Jannou <jannoub@gmail.com>
2016-03-23 16:37:30 +0100

    modification ArcTask ( effectuer les calcule mais ne pas rajouter les arc dans le graph, on les met dans une file qui est gerer par le thread principal a la fin de tout les calculs )

--- src/main/java/semiGlobal/ArcTask.java
+++ src/main/java/semiGlobal/ArcTask.java
@@ -3,6 +3,8 @@ package semiGlobal;
 import overlap.Arc;
 import overlap.Overlap;
 
+import java.util.Queue;
+
 /**
  * tache pour calcule les alignement entre un sommet T et tout les autres sommmet avec les quelle le sommet T n as pas encore ete compare
  * @author Jannou Brohee
@@ -11,6 +13,8 @@ public class ArcTask {
 	int sommetIndex;
 	Overlap graph;
 	Alignement al;
+	ComputeThread thread;
+	Queue<Arc> topush;
 
 	/**
 	 * Constructeur
@@ -18,10 +22,12 @@ public class ArcTask {
 	 * @param _graph le graph
 	 * @param _al l alignement
 	 */
-	public ArcTask(int _sommetIndex, Overlap _graph, Alignement _al){
+	public ArcTask(int _sommetIndex, Overlap _graph, Alignement _al,ComputeThread _thread,Queue<Arc> _topush){
+		topush=_topush;
 		sommetIndex = _sommetIndex;
 		al = _al;
 		graph = _graph;
+		thread=_thread;
 	}
 
 	/**
@@ -29,21 +35,24 @@ public class ArcTask {
 	 */
 	public void run(){
 		int[] score;
-		for(int i = sommetIndex ;i<graph.getSommets().size();i++){
-			if(i!= sommetIndex){
-				score = al.getBestScores(graph.getSommet(sommetIndex).getFrag(), graph.getSommet(i).getFrag());
-				graph.addArc(new Arc(i, sommetIndex ,false, false, score[0]));
-				graph.addArc(new Arc(sommetIndex , i, false, false, score[1]));
-				score = al.getBestScores(graph.getSommet(sommetIndex).getFrag(), graph.getSommet(i).getFrag().getComplementaire());
-				graph.addArc( new Arc(i, sommetIndex ,false, true, score[0]));
-				graph.addArc( new Arc(sommetIndex , i, true, false,score[1]));
-				score = al.getBestScores(graph.getSommet(sommetIndex).getFrag().getComplementaire(), graph.getSommet(i).getFrag());
-				graph.addArc( new Arc(i, sommetIndex ,true, false, score[0]));
-				graph.addArc( new Arc(sommetIndex , i, false, true,score[1]));
-				score = al.getBestScores(graph.getSommet(sommetIndex).getFrag().getComplementaire(), graph.getSommet(i).getFrag().getComplementaire());
-				graph.addArc( new Arc(i, sommetIndex ,true,true, score[0]));
-				graph.addArc( new Arc(sommetIndex , i, true,true ,score[1]));
-			}
+		for(int i = sommetIndex+1 ;i<graph.getSommets().size();i++){
+			score = al.getBestScores(graph.getSommet(sommetIndex).getFrag(), graph.getSommet(i).getFrag());
+			topush.add(new Arc(i, sommetIndex ,false, false, score[0]));
+			topush.add(new Arc(sommetIndex , i, false, false, score[1]));
+			score = al.getBestScores(graph.getSommet(sommetIndex).getFrag(), graph.getSommet(i).getFrag().getComplementaire());
+			topush.add( new Arc(i, sommetIndex ,false, true, score[0]));
+			topush.add( new Arc(sommetIndex , i, true, false,score[1]));
+			score = al.getBestScores(graph.getSommet(sommetIndex).getFrag().getComplementaire(), graph.getSommet(i).getFrag());
+			topush.add( new Arc(i, sommetIndex ,true, false, score[0]));
+			topush.add( new Arc(sommetIndex , i, false, true,score[1]));
+			score = al.getBestScores(graph.getSommet(sommetIndex).getFrag().getComplementaire(), graph.getSommet(i).getFrag().getComplementaire());
+			topush.add( new Arc(i, sommetIndex ,true,true, score[0]));
+			topush.add( new Arc(sommetIndex , i, true,true ,score[1]));
 		}
+		upp();
 	}
+	private void upp(){
+		thread.upp();
+	}
+
 }
