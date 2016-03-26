18964f305d1fdc2a084616886ea718262711950b
Jannou <jannoub@gmail.com>
2016-03-26 10:12:43 +0100

    implementation aligne( Fragment G, Fragment T) qui retourne l alignement de T sur G ou T  " s adapte" a G

--- src/main/java/semiGlobal/Alignement.java
+++ src/main/java/semiGlobal/Alignement.java
@@ -102,6 +102,7 @@ public class Alignement {
     public byte getGap(){
         return gap;
     }
+
 	public int[] getBestScore( int[][] matrice,Fragment s, Fragment t){
 		int[] returne = new int[2];
 		int score=0;
@@ -125,7 +126,7 @@ public class Alignement {
 		int[] retour = new int[2];
 		int[][] matrice = matriceSim(G,T);
 		int tmpscore=0;
-		int iG = 0,iT=0,jG = 0,jT=0; // iS,jS ==> indice quand on commence par S
+		int iG = 0,iT=0,jG = 0,jT=0; // iG,jG ==> indice quand on commence par G
 		for(int j=1;j<=T.length();j++){
 			if(matrice[G.length()][j]>tmpscore){
 				tmpscore = matrice[G.length()][j];
@@ -181,40 +182,69 @@ public class Alignement {
 		}
 		return retour;
 	}
-	// score ici
-	public Fragment aligne( Fragment s, Fragment t){
-		String ret= "";
-		int[][] matrice = matriceSim(s,t); //(O(n*m)
-		int[] tmp = getBestScore(matrice,s,t); //(O(m+n))
-		int ib = tmp[0];
-		int jb = tmp[1];
-		//differencier si best est trouver en fin de S ou en fin de T
-		if(ib==s.length()) // best en fin de S => copier partie jb->m de t    0__________jb_________m
-			ret = t.substring(jb); // O(1)
-		if(jb == t.length()) // best en fin de T => copier partie ib->n de s    0__________ib_________n
-			ret = s.substring(ib); // O(1)
-		while(jb !=0 && ib !=0){
-			if(matrice[ib][jb] == matrice[ib-1][jb]+getGap()){ // gap en s
-				ret = s.get(ib-1) + ret;
-				ib--;
-			}
-			else if(matrice[ib][jb] == matrice[ib][jb-1]+getGap()){ // gap en t
-				ret = t.get(jb-1) + ret;
-				jb --;
-			}
-			else if(matrice[ib][jb] == matrice[ib - 1][jb - 1] + getMatchScore(s.get(ib - 1), t.get(jb - 1))) {
-				if (getMatchScore(s.get(ib - 1), t.get(jb - 1)) == getMatch())
-					ret = s.get(ib - 1) + ret;
-				else
-					ret = "F" + ret;
-				ib--;
-				jb--;
+
+	/**
+	 * Retourne un tableau de String de taille 2 representant l alignement de T sur G ou on maximise les gap a la fin de G et au debut de T.
+	 * Le premier element du tableau represente G et le deuxieme element represente T .
+	 * @param G Le fragment ou on maximise les gap a la fin
+	 * @param T Le fragment ou on maximise les gap au debut
+	 * @return Un tableau de String de taille 2 ou le premier element represente G et le deuxieme element represente T .
+	 */
+	public String[] aligne( Fragment G, Fragment T){
+		String retour[] = new String[2];
+		retour[0] = retour[1] = "";
+		int[][] matrice = matriceSim(G,T);
+		int tmpscore=0;
+		int iG = 0,jG = 0;
+		for(int j=1;j<=T.length();j++){
+			if(matrice[G.length()][j]>tmpscore){
+				tmpscore = matrice[G.length()][j];
+				iG=G.length();
+				jG=j;
 			}
 		}
-		if(ib!=0) // best en fin de S => copier partie jb->m de t    0__________jb_________m
-			ret = s.substring(0).substring(0,ib-1)+ret;
-		if(jb!=0) // best en fin de T => copier partie ib->n de s    0__________ib_________n
-			ret = t.substring(0).substring(0,jb-1)+ret;
-		return new Fragment(ret);
+
+		// comme on aligne un fragment sur un autre on sait que l'un des deux fragment ne sera pas modifier,
+		// on cherche donc a construire le deuxieme fragment qui a ete modifier pour s aligner
+		// ==> on a donc T.lenght - jG gap a mettre pour G
+		//==> on doit recuperer les T.lenght - jG dernier element de T
+		for(int i = 0; i<(T.length()-jG);i++ )
+			retour[0] += "_";
+		retour[1] = T.substring(jG); // O(1)
+		// on construit les alignement
+		while(jG !=0 && iG !=0){
+			if(matrice[iG][jG] == matrice[iG-1][jG]+getGap()){ // gap en G + recuperer une base dans T
+				retour[0] = G.get(iG-1) + retour[0];
+				retour[1] = "_"+retour[1];
+				iG--;
+			}
+			else if(matrice[iG][jG] == matrice[iG][jG-1]+getGap()){ // gap en T + recuperer une base dans G
+				retour[0] = "_"+retour[0];
+				retour[1] = T.get(jG) + retour[1];
+				jG --;
+			}
+			else if(matrice[iG][jG] == matrice[iG - 1][jG - 1] + getMatchScore(G.get(iG - 1), T.get(jG - 1))) { //on copie un base de G et une base de T
+				retour[1] = T.get(jG-1) + retour[1];
+				retour[0] = G.get(iG-1) + retour[0];
+				iG--;
+				jG--;
+			}
+		}
+		// 2 cas possible,
+		if(iG!=0){ // il reste des base dans G mais pas dans T ==> rajouter des gap dans T (chevauchement )
+			while(iG>0){
+				retour[0] = G.get(iG-1) + retour[0];
+				retour[1] = "_"+retour[1];
+				iG--;
+			}
+		}
+		if(jG!=0){ // il reste des base dans T mais pas dans G ==> rajouter des gap dans G (G est "inclus" dans T )
+			while (jG > 0) {
+				retour[0] = "_" +retour[0];
+				retour[1] = T.get(jG-1) + retour[1];
+				jG--;
+			}
+		}
+		return retour;
 	}
 }
